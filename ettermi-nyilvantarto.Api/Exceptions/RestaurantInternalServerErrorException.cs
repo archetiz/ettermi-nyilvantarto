edaa53629c9ef6ce13945abf2c77e382e067f947
Kókai Kristóf <archetiz@sch.bme.hu>
2020-11-23 13:54:56 +0100

    Custom 500 error

---
+++ ettermi-nyilvantarto.Api/Exceptions/RestaurantInternalServerErrorException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ettermi_nyilvantarto.Api
+{
+	class RestaurantInternalServerErrorException : RestaurantException
+	{
+		public override int StatusCode { get; set; } = 500;
+		public RestaurantInternalServerErrorException()
+		{
+		}
+
+		public RestaurantInternalServerErrorException(string message) : base(message)
+		{
+		}
+
+		public RestaurantInternalServerErrorException(string message, Exception inner) : base(message, inner)
+		{
+		}
+	}
+}
