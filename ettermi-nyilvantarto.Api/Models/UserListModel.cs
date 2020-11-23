b45d28e891c0482f8f78f64abb7f49d25d0e0886
Kókai Kristóf <archetiz@sch.bme.hu>
2020-11-23 13:56:18 +0100

    User API extended

---
+++ ettermi-nyilvantarto.Api/Models/UserListModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ettermi_nyilvantarto.Api
+{
+	public class UserListModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string UserName { get; set; }
+		public string AccountType { get; set; }
+	}
+}
